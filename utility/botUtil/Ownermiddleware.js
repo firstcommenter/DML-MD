497768d080ccca5d97c5f56a7c198067968458e6
FezChat <frediezra001@gmail.com>
2026-01-04 13:40:00 +0300

    Upload DML-MD complete project

---
+++ utility/botUtil/Ownermiddleware.js
@@ -0,0 +1,18 @@
+const Ownermiddleware = async (context, next) => {
+    const { m, Owner } = context;
+
+    if (!Owner) {
+        return m.reply(`╭┈┈┈┈━━━━━━┈┈┈┈◈
+┋❒ You dare attempt to use Owner command? 
+
+┋❒ Your mere existence is an insult to my code. 
+
+┋❒ Crawl back into the abyss where mediocrity thrives before I personally wipe you from this reality fool. 💀👿
+╰┈┈┈┈━━━━━━┈┈┈┈◈
+> REGARDS DML TECH `);
+    }
+
+    await next();
+};
+
+module.exports = Ownermiddleware;
