25c5dc6ed444a873853d6239fc497a2b1995eaf3
DML <daudymussa1705@gmail.com>
2026-01-07 19:16:39 +0300

    Update vvx.js

--- dmlplugins/Groups/vvx.js
+++ dmlplugins/Groups/vvx.js
@@ -23,7 +23,7 @@ module.exports = async (context) => {
         const buffer = fs.readFileSync(returnedPath);
         const chatId = m.chat || client.user?.id;
         
-        const caption = `🥀\n—\n🄵🄴🄴-🅇🄼🄳`;
+        const caption = `🔰\n—\nDML-MD`;
 
         if (imageMsg) {
             await client.sendMessage(chatId, { image: buffer, caption }, { quoted: m });
