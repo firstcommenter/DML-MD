1a93f333aa896f463f6e0c105bc621b0eb8e8899
DML <daudymussa1705@gmail.com>
2026-01-10 07:50:18 -0800

    Update support.js

--- dmlplugins/General/support.js
+++ dmlplugins/General/support.js
@@ -14,7 +14,7 @@ module.exports = async (context) => {
 
     try {
         const replyText =
-            `╭❖🤝 ${botname} Support & Official Links❖╮\n` +
+            `╭${botname} Support & Official Links╮\n` +
             `│\n` +
             `│  Stay connected with our official platforms\n` +
             `│  to receive:\n` +
