2d17502511b31953016649886af4fbea75759052
MLILA MUSA <daudymussa1705@gmail.com>
2026-03-30 08:30:26 -0700

    Update script.js

--- dmlplugins/General/script.js
+++ dmlplugins/General/script.js
@@ -43,7 +43,7 @@ module.exports = async (context) => {
                             name: 'cta_url',
                             buttonParamsJson: JSON.stringify({
                                 display_text: '🔐 View Pair Code',
-                                url: 'https://dml-pairing-59op.onrender.com'
+                                url: 'https://session.dml-tech.online'
                             })
                         },
                         {
